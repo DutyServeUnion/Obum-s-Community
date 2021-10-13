05702eb85bcffeb4485a6ca9096b26e66f227992
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-13 14:59:55 +0400

    background+borders+Sections

--- garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obum_hud_v1/obum_hud_1/cl_main.lua
+++ garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obum_hud_v1/obum_hud_1/cl_main.lua
@@ -62,4 +62,20 @@ HUD.HHeight = HUD.Height / 2 + 11
 
 local function Base()
 
-      
+		-- back ground
+	draw.RoundedBox(3, HUD.PosX-2, HUD.posY-2, HUD.Width+4, HUD.Height+4, Color(30, 30, 30,255))
+	draw.RoundedBox(3, HUD.PosX, HUD.PosY, HUD.Width, HUD.Height, Color(50,50,50,255))
+		-- borders
+      draw.RoundedBox(0, HUD.PosX, HUD.BPosY-4, HUD.Width, 2, Color(30,30,30,255))
+	draw.RoundedBox(0, HUD.PosX, HUD.BPosY-2, HUD.Width, 2, Color(90,90,90,255))
+	
+	draw.RoundedBox(0, HUD.PosX + HUD.Width - 50 - 4, HUD.PosY, 2, HUD.HHeight, Color(30,30,30,255))
+	draw.RoundedBox(0, HUD.PosX + HUD.Width - 50 - 2, HUD.PosY, 2, HUD.HHeight, Color(90,90,90,255))
+	
+	draw.RoundedBox(0, HUD.PosX + 75 + 2, HUD.PosY, 2, HUD.HHeight, Color(30,30,30,255))
+	draw.RoundedBox(0, HUD.PosX + 75, HUD.PosY, 2, HUD.HHeight, Color(90,90,90,255))
+		
+		-- sections
+	draw.RoundedBoxEx(4, HUD.PosX, HUD.BPosY, HUD.Width, HUD.BHeight, Color(70,70,70,255), false, false, true, true)
+	draw.RoundedBoxEx(4, HUD.PosX + HUD.Width - 50, HUD.PosY, 50, HUD.HHeight, Color(60,60,60,255), false, true, false, false)
+	draw.RoundedBoxEx(4, HUD.PosX, HUD.PosY, 75, HUD.HHeight, Color(70,70,70,255), true, false, false, false)		

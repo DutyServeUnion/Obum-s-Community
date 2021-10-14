b02daad2a1d184ba932d045d4344c3c04c2217ae
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-14 10:26:50 +0400

    ceee elll font dot looo waaa is done

---
+++ garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obum_hud_v1/obum_hud_1/cl_font.lua
@@ -0,0 +1,35 @@
+-- babaoey font (very xd gaming)
+
+urface.CreateFont( "Obum_Roboto_1", {
+    font = "Roboto",    
+	size = 20,
+	weight = 150,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = true,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
+
+surface.CreateFont( "Obum_Roboto_2", {
+    font = "Roboto",    
+	size = 20,
+	weight = 150,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = true,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})

f2fdd26b877a41503aa3b621f75905e195b2a5a5
DutyServeUnion <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-14 10:15:44 +0400

    format function and other fixes
    cl_main.lua lol

--- garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obum_hud_v1/obum_hud_1/cl_main.lua
+++ garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obum_hud_v1/obum_hud_1/cl_main.lua
@@ -48,6 +48,19 @@ else
    HUD.PosX = HUD.Border
 end
 	
+	-- Format Number Func --/
+local function formatNumber(n)
+	if not n then return "" end
+	if n >= 1e14 then return tostring(n) end
+	n = tostring(n)
+	local sep = sep or ","
+	local dp = string.find(n, "%.") or #n+1
+	for i=dp-4, 1, -3 do
+		n = n:sub(1, i) .. sep .. n:sub(i+1)
+	end
+	return n
+end
+	
 	-- hud element sjit --
 HUD.BHeight = HUD.Height / 2 - 15
 HUD.BPosY = HUD.PosY + HUD.Height / 2 + HUD.Border
@@ -63,19 +76,21 @@ HUD.HHeight = HUD.Height / 2 + 11
 local function Base()
 
 		-- back ground
-	draw.RoundedBox(3, HUD.PosX-2, HUD.posY-2, HUD.Width+4, HUD.Height+4, Color(30, 30, 30,255))
-	draw.RoundedBox(3, HUD.PosX, HUD.PosY, HUD.Width, HUD.Height, Color(50,50,50,255))
+		draw.RoundedBox(4, HUD.PosX-1, HUD.PosY-1, HUD.Width+2, HUD.Height+2, Color(30,30,30,255))
+		draw.RoundedBox(4, HUD.PosX, HUD.PosY, HUD.Width, HUD.Height, Color(50,50,50,255))
 		-- borders
-      draw.RoundedBox(0, HUD.PosX, HUD.BPosY-4, HUD.Width, 2, Color(30,30,30,255))
-	draw.RoundedBox(0, HUD.PosX, HUD.BPosY-2, HUD.Width, 2, Color(90,90,90,255))
-	
-	draw.RoundedBox(0, HUD.PosX + HUD.Width - 50 - 4, HUD.PosY, 2, HUD.HHeight, Color(30,30,30,255))
-	draw.RoundedBox(0, HUD.PosX + HUD.Width - 50 - 2, HUD.PosY, 2, HUD.HHeight, Color(90,90,90,255))
-	
-	draw.RoundedBox(0, HUD.PosX + 75 + 2, HUD.PosY, 2, HUD.HHeight, Color(30,30,30,255))
-	draw.RoundedBox(0, HUD.PosX + 75, HUD.PosY, 2, HUD.HHeight, Color(90,90,90,255))
+		draw.RoundedBox(0, HUD.PosX, HUD.BPosY-4, HUD.Width, 2, Color(30,30,30,255))
+		draw.RoundedBox(0, HUD.PosX, HUD.BPosY-2, HUD.Width, 2, Color(90,90,90,255))
+		
+		draw.RoundedBox(0, HUD.PosX + HUD.Width - 50 - 4, HUD.PosY, 2, HUD.HHeight, Color(30,30,30,255))
+		draw.RoundedBox(0, HUD.PosX + HUD.Width - 50 - 2, HUD.PosY, 2, HUD.HHeight, Color(90,90,90,255))
+		
+		draw.RoundedBox(0, HUD.PosX + 75 + 2, HUD.PosY, 2, HUD.HHeight, Color(30,30,30,255))
+		draw.RoundedBox(0, HUD.PosX + 75, HUD.PosY, 2, HUD.HHeight, Color(90,90,90,255))
 		
 		-- sections
-	draw.RoundedBoxEx(4, HUD.PosX, HUD.BPosY, HUD.Width, HUD.BHeight, Color(70,70,70,255), false, false, true, true)
-	draw.RoundedBoxEx(4, HUD.PosX + HUD.Width - 50, HUD.PosY, 50, HUD.HHeight, Color(60,60,60,255), false, true, false, false)
-	draw.RoundedBoxEx(4, HUD.PosX, HUD.PosY, 75, HUD.HHeight, Color(70,70,70,255), true, false, false, false)		
+		draw.RoundedBoxEx(4, HUD.PosX, HUD.BPosY, HUD.Width, HUD.BHeight, Color(70,70,70,255), false, false, true, true)
+		draw.RoundedBoxEx(4, HUD.PosX + HUD.Width - 50, HUD.PosY, 50, HUD.HHeight, Color(60,60,60,255), false, true, false, false)
+		draw.RoundedBoxEx(4, HUD.PosX, HUD.PosY, 75, HUD.HHeight, Color(70,70,70,255), true, false, false, false)	
+		end
+

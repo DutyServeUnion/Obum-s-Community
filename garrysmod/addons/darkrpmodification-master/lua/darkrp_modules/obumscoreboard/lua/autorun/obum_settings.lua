34a292ede2cdaffed4ceeb7ea2767ea133a60cc7
Andrew <84892309+DutyServeUnion@users.noreply.github.com>
2021-11-04 07:42:30 +0400

    Update obum_settings.lua

--- garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obumscoreboard/lua/autorun/obum_settings.lua
+++ garrysmod/addons/darkrpmodification-master/lua/darkrp_modules/obumscoreboard/lua/autorun/obum_settings.lua
@@ -1,15 +1,18 @@
--- =======================SETTINGS=======================
+-- =======================SETTINGS======================= --
 
-ulx = true              --Ulx/Ulib
+-- really unsure about this so, I got the ranks from the file however the 'serverguard = true is 100% wrong
+
+serverguard = true              --Ulx/Ulib/ServerGuard
 
 ModerationGroups = { -- Who can see the moderation panel
+        ['founder ] = true
         [ 'superadmin' ] = true,
         [ 'admin' ] = true
         [ 'moderator' ] = true 
     }
 
 GroupsColors = {
-    [ 'superadmin' ] = { color = Color( 240, 52, 52, 230 ) },
+    [ 'superadmin' ] = { color = Color( 255, 3, 36 ) },
     [ 'admin' ] = { color = Color( 240, 52, 52, 230 ) },
     [ 'moderator' ] = { color = Color( 240, 52, 52, 230 ) },
     [ 'user' ] = { color = Color( 83, 51, 237, 230 ) },
